2c91e24cbc3d8a07472ef4fe5339d44feb6ec454
Zeesshan Jabbar <zeeshanch124@gmail.com>
2026-05-08 10:38:27 +0100

    print directory

--- ModelCleaning/train_test_dataset.py
+++ ModelCleaning/train_test_dataset.py
@@ -32,4 +32,5 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 mind = LinearRegression()
 mind.fit(X_train, y_train)
 
+print("Current Working Directory:", os.getcwd())
 dump(mind, "AgeSalaryModel.pkl")
